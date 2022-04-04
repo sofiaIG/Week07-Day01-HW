80d72f6d933c9c5f72357f51c2d3b5622dd24c4c
sofiaIG <user@users-MacBook-Pro.local>
2022-04-04 15:10:14 +0100

    mvp done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [activities, setActivities] = useState(
+    ["Buy shopping", "Clean bathroom", "Car's MOT"]
+  );
+
+  const activityNodes = activities.map((activity) => {
+    return(
+          <li>
+            <span>{activity}</span>
+      
+          </li>
+  )})
+
+  const saveNewActivity = (event) =>{
+    event.preventDefault();
+    const copyActivities = [...activities];
+    copyActivities.push(newActivity);
+    setActivities(copyActivities);
+    setNewActivity('');
+  }
+
+
+
+  const [newActivity, setNewActivity] = useState('');
+
+
+  const handleActivityInput = (event) => {
+    setNewActivity(event.target.value);}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <h1>To Do List</h1>
+    <hr></hr>
+    <form onSubmit ={saveNewActivity}>
+        <label htmlFor = 'new-activity'>Save activity:</label>
+        {/* <input type="radio" id="high" name="high_priority" value="high_priority"/>
+        <label for="html">High</label>
+        <input type="radio" id="low" name="low_priority" value="low_priority"/>
+        <label for="html">Low</label> */}
+        <input id = "new-activity" type = 'text' value = {newActivity} onChange = {handleActivityInput}/>
+        <input type = 'submit' value = 'Save New Activity'/>
+
+      </form>
+    <ul>
+      <li>{activityNodes}</li>
+    </ul>
     </div>
+    
+
+
+
   );
 }
 
